b1640cca1f92c73152371f24a4a757dd75161993
hamiltonmatematica <hamilton.vinicius@gmail.com>
2025-12-31 17:48:02 -0300

    Fix: Removido w-full do botão Pricing

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -26,7 +26,7 @@ export const Pricing: React.FC = () => {
 
           <div className="text-center">
             <a href="https://pay.kiwify.com.br/lTezrlP" target="_blank" rel="noopener noreferrer">
-              <button className="cta-button py-5 px-4 md:px-6 rounded-xl text-lg md:text-xl font-bold uppercase tracking-wider shadow-2xl shadow-emerald-500/40 hover:shadow-emerald-500/60 transition-all w-full">
+              <button className="cta-button py-5 px-4 md:px-6 rounded-xl text-lg md:text-xl font-bold uppercase tracking-wider shadow-2xl shadow-emerald-500/40 hover:shadow-emerald-500/60 transition-all">
                 Quero organizar meu dinheiro
               </button>
             </a>
